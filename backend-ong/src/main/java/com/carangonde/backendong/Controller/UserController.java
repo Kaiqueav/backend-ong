049f13604b26aae576e54c813fa332af2e541aa6
kaiqueav <kaiquecerq66@outlook.com>
2024-06-04 21:27:32 -0300

    feat: create get router

--- backend-ong/src/main/java/com/carangonde/backendong/Controller/UserController.java
+++ backend-ong/src/main/java/com/carangonde/backendong/Controller/UserController.java
@@ -1,21 +1,27 @@
 package com.carangonde.backendong.Controller;
 
 import com.carangonde.backendong.Model.User;
+import com.carangonde.backendong.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/user")
 @RestController
 public class UserController {
 
     @Autowired
-
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@RequestBody User user){
-        User  userSave = userService;
+        private UserService userService;
+    @PostMapping
+    ResponseEntity<User> createUser(@RequestBody User user){
+        User newUser = userService.registerUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
-}
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        User user = userService.getUserId(id);
+        return ResponseEntity.ok(user);
+    }
+
+
